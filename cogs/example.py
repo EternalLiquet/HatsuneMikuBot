15854edd084a761e48032e94cd4e6dd79dc565df
Gary Ng <garyng@garys-mbp.attlocal.net>
2021-02-07 11:10:10 -0500

    cogs folder

---
+++ cogs/example.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+import random
+
+
+class Example(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('bot is online')
+
+
+    @commands.command(aliases=['8ball'])
+    async def eight_ball(self, ctx, *, question):
+        responses = ["It is certain.",
+                    "It is decidedly so.",
+                    "Without a doubt.",
+                    "Yes - definitely.",
+                    "You may rely on it.",
+                    "As I see it, yes.",
+                    "Most likely.",
+                    "Outlook good.",
+                    "Yes.",
+                    "Signs point to yes.",
+                    "Reply hazy, try again.",
+                    "Ask again later.",
+                    "Better not tell you now.",
+                    "Cannot predict now.",
+                    "Concentrate and ask again.",
+                    "Don't count on it.",
+                    "My reply is no.",
+                    "My sources say no.",
+                    "Outlook not so good.",
+                    "Very doubtful."]
+        await ctx.send(f'Question: {question}\n Answer: {random.choice(responses)} ')
+
+def setup(client):
+    client.add_cog(Example(client))
+
