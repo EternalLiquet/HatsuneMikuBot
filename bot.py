0bc61801ef04d43fe8a1a63909242cbfc3413442
Gary Ng <garyng@garys-mbp.attlocal.net>
2021-02-07 11:08:42 -0500

    update bot.py locally

--- bot.py
+++ bot.py
@@ -11,4 +11,33 @@ async def ping(ctx):
     await ctx.send('pong')
 TOKEN = os.getenv('token')
 
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension}')
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+
+@bot.command()
+async def reload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+    bot.load_extension(f'cogs.{extension}')
+
+@bot.event
+async def on_message(message):
+    if 'all in?' in message.content:
+        channel = message.channel
+        await channel.send("Tesla!")
+    elif 'Tesla!' in message.content:
+        channel = message.channel
+        await channel.send("all in?")
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+
+
 bot.run(TOKEN)
