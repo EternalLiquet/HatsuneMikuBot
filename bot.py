2d64e61d00414130f16417b765d300a57f3a9712
Raul Guajardo <hitomisparda@gmail.com>
2021-02-07 05:33:53 -0500

    Sleepy Miku <3

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ async def ping(ctx):
 async def on_message(message):
     moment = datetime.datetime.now()
     if moment.hour >= 2 or moment.hour <= 5:
-        await message.channel.send('It\'s time for beddy byes, {}, you need your beauty sleep <3.'.format(message.author.name))
+        await message.channel.send('It\'s time for beddy byes, {}, you need your beauty sleep <3. \nhttps://i.pinimg.com/originals/de/f3/2b/def32b29236bebf71e878e4bab16f278.jpg'.format(message.author.name))
 
 TOKEN = os.getenv('token')
 
